d13abce6430fcc9ac7f4ff7de2c7659acd52e9e4
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-03 18:51:49 +0530

    Add Firebase Authentication dependencies and Google Services plugin.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -61,4 +62,6 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.bundles.credentials)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
 }
