64f808037103c0d8b56a11fa32e315169bcb0a77
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-04 10:42:00 +0530

    Add Login Screen and configure navigation.

--- app/src/main/java/com/evaluation/githubrepository/navigation/GithubRepositoryNavHost.kt
+++ app/src/main/java/com/evaluation/githubrepository/navigation/GithubRepositoryNavHost.kt
@@ -9,18 +9,28 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.evaluation.githubrepository.presentation.login.LoginScreen
 
 @Composable
 fun GithubRepositoryNavHost(
     navController: NavHostController,
     modifier: Modifier = Modifier,
-    startDestination: Screen = Screen.Home,
+    startDestination: Screen = Screen.Login,
 ) {
     NavHost(
         navController = navController,
         startDestination = startDestination,
         modifier = modifier,
     ) {
+        composable<Screen.Login> {
+            LoginScreen(
+                onLoginComplete = {
+                    navController.navigate(Screen.Home) {
+                        popUpTo(Screen.Login) { inclusive = true }
+                    }
+                },
+            )
+        }
         composable<Screen.Home> {
             Box(
                 modifier = Modifier.fillMaxSize(),
--- app/src/main/java/com/evaluation/githubrepository/navigation/Screen.kt
+++ app/src/main/java/com/evaluation/githubrepository/navigation/Screen.kt
@@ -4,5 +4,8 @@ import kotlinx.serialization.Serializable
 
 sealed class Screen {
     @Serializable
+    data object Login : Screen()
+
+    @Serializable
     data object Home : Screen()
 }
