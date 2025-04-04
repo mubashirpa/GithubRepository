52c6b3b9dcfd5608681db343068f7d9f04fa3b73
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-04 12:20:19 +0530

    Implement the home screen.

--- app/src/main/java/com/evaluation/githubrepository/navigation/GithubRepositoryNavHost.kt
+++ app/src/main/java/com/evaluation/githubrepository/navigation/GithubRepositoryNavHost.kt
@@ -1,14 +1,11 @@
 package com.evaluation.githubrepository.navigation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.evaluation.githubrepository.presentation.home.HomeScreen
 import com.evaluation.githubrepository.presentation.login.LoginScreen
 
 @Composable
@@ -32,12 +29,7 @@ fun GithubRepositoryNavHost(
             )
         }
         composable<Screen.Home> {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center,
-            ) {
-                Text(text = "Coming Soon!")
-            }
+            HomeScreen()
         }
     }
 }
