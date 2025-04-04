6874ce5821a621a0dbc0ff8fdf01baa3ff337980
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-04 10:31:35 +0530

    Add Koin dependency injection module.

--- app/src/main/java/com/evaluation/githubrepository/di/AppModule.kt
+++ app/src/main/java/com/evaluation/githubrepository/di/AppModule.kt
@@ -1,9 +1,19 @@
 package com.evaluation.githubrepository.di
 
 import com.google.firebase.auth.FirebaseAuth
+import com.evaluation.githubrepository.data.repository.AuthenticationRepositoryImpl
+import com.evaluation.githubrepository.domain.repository.AuthenticationRepository
+import com.evaluation.githubrepository.domain.usecase.LoginWithGoogleUseCase
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val appModule =
     module {
         single { FirebaseAuth.getInstance() }
+        single { Firebase.auth }
+        singleOf(::AuthenticationRepositoryImpl) { bind<AuthenticationRepository>() }
+        single { LoginWithGoogleUseCase(get()) }
     }
