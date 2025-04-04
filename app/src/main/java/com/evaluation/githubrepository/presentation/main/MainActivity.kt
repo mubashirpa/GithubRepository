f0c427ffe62cc56949e3f4e731e4123d8b59afd5
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-04 11:11:44 +0530

    Add a splash screen and a loading screen.

--- app/src/main/java/com/evaluation/githubrepository/presentation/main/MainActivity.kt
+++ app/src/main/java/com/evaluation/githubrepository/presentation/main/MainActivity.kt
@@ -8,24 +8,41 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.evaluation.githubrepository.navigation.GithubRepositoryNavHost
+import com.evaluation.githubrepository.navigation.Screen
+import com.evaluation.githubrepository.presentation.components.LoadingScreen
 import com.evaluation.githubrepository.presentation.theme.GithubRepositoryTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        splashScreen.setKeepOnScreenCondition { viewModel.uiState.isLoading }
+
         enableEdgeToEdge()
         setContent {
             GithubRepositoryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GithubRepositoryNavHost(
-                        navController = rememberNavController(),
-                        modifier =
-                            Modifier
-                                .fillMaxSize()
-                                .padding(innerPadding),
-                    )
+                    if (viewModel.uiState.isLoading) {
+                        LoadingScreen(modifier = Modifier.padding(innerPadding))
+                    } else {
+                        val startDestination =
+                            if (viewModel.uiState.isUserLoggedIn) Screen.Home else Screen.Home
+                        GithubRepositoryNavHost(
+                            navController = rememberNavController(),
+                            modifier =
+                                Modifier
+                                    .fillMaxSize()
+                                    .padding(innerPadding),
+                            startDestination = startDestination,
+                        )
+                    }
                 }
             }
         }
