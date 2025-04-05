1f4aca9b57655c36557cc89ea695698f44edbdfe
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-05 12:58:21 +0530

    Add functions for mapping between DTO, entity, and domain models.

--- app/src/main/java/com/evaluation/githubrepository/data/mapper/RepoMapper.kt
+++ app/src/main/java/com/evaluation/githubrepository/data/mapper/RepoMapper.kt
@@ -1,11 +1,28 @@
 package com.evaluation.githubrepository.data.mapper
 
+import com.evaluation.githubrepository.data.local.entity.ReposEntity
 import com.evaluation.githubrepository.data.remote.dto.github.repos.RepoDto
 import com.evaluation.githubrepository.data.remote.dto.github.search.SearchRepoDto
 import com.evaluation.githubrepository.domain.model.github.repos.Repo
 import com.evaluation.githubrepository.domain.model.github.search.SearchRepo
 
 fun RepoDto.toRepo(): Repo =
+    Repo(
+        description = description,
+        id = id,
+        name = name,
+        stargazersCount = stargazersCount,
+    )
+
+fun RepoDto.toRepoEntity(): ReposEntity =
+    ReposEntity(
+        id = id!!,
+        description = description,
+        name = name,
+        stargazersCount = stargazersCount,
+    )
+
+fun ReposEntity.toRepo(): Repo =
     Repo(
         description = description,
         id = id,
