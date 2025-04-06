521d0ecd3873a7828c70239b87a0086441a6613a
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-06 18:32:05 +0530

    Feat: Add notification channel for application.
    - Created a notification channel for the application to handle notifications effectively.
    - Implemented the channel with default importance and provided a name and description for user clarity.
    - Registered the channel with the system's NotificationManager.

--- app/src/main/java/com/evaluation/githubrepository/GithubRepositoryApplication.kt
+++ app/src/main/java/com/evaluation/githubrepository/GithubRepositoryApplication.kt
@@ -1,6 +1,10 @@
 package com.evaluation.githubrepository
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.evaluation.githubrepository.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
@@ -13,5 +17,22 @@ class GithubRepositoryApplication : Application() {
             androidContext(this@GithubRepositoryApplication)
             modules(appModule)
         }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel() {
+        val channelId = getString(R.string.default_notification_channel_id)
+        val channelName = getString(R.string.default_notification_channel_name)
+        val channelDescription = getString(R.string.default_notification_channel_description)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channelId, channelName, importance)
+        channel.description = channelDescription
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
